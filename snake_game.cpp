751fa619cba21c96435d49143aa7f4051b2c659c
Thasuni Induma <thasuniinduma@gmail.com>
2024-08-24 13:42:41 +0530

    complete

--- snake_game.cpp
+++ snake_game.cpp
@@ -1,171 +1,198 @@
 #include <iostream>
 #include <conio.h>
 #include <windows.h>
-#define MAXSNAKESIZE 100
-#include <dos.h>
-#include <time.h>
+#include <cstdlib>
 
 using namespace std;
 
-HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
-COORD CursorPosition;
+//hight and width
+const int width = 50;
+const int height = 20;
 
-void gotoxy(int x, int y){
-	CursorPosition.X=x;
-	CursorPosition.Y=y;
-	SetConsoleCursorPosition(console,CursorPosition);
+//snake head 
+int x,y;
+//food
+int fruitX,fruitY;
+//store score of the player
+int playerScore;
+//snake tail
+int snakeTailX[100],snakeTailY[100];
+//store length of the snake tail
+int snakeTailLength;
+//snake move
+enum snakeDir{STOP = 0, LEFT, RIGHT, UP, DOWN};
+snakeDir sDir;
+//checking game over or not
+bool isGameOver;
+
+//initialize game variables
+void Game(){
+	isGameOver = false;
+	sDir = STOP;
+	x = width/2;
+	y=height/2;
+	fruitX = rand()%width;
+	fruitY = rand()%height;
+	playerScore = 0;
 }
 
-class Point{
-	private:
-			int x;
-			int y;
-	public: 
-	Point(){
-		x=y=10;
-	}
-	Point(int x, int y){
-		this -> x=x;
-		this -> y=y;
-	}
-	void SetPoint(int x, int y){
-		this -> x=x;
-		this -> y=y;
-	}
-	int GetX(){
-		return x;
-	}
-	int GetY(){
-		return y;
-	}
-	void MoveUp(){
-		y--;
-	}
-	void MoveDown(){
-		y++;
-	}
-	void MoveLeft(){
-		x--;
-	}
-	void MoveRight(){
-		x++;
-	}
-	void Draw(){
-		gotoxy(x,y);
-		cout<<"*";
-	}
-	void Erase(){
-		gotoxy(x,y);
-		cout<<" ";
-	}
-	void CopyPos(Point *p){
-		p -> x=x;
-		p -> y=y;
-	}
-	void Debug(){
-		cout<<"("<<x<<","<<y<<")";
-	}
-};
+//create the game board 
+void GameRender(string playerName){
+	//clear console
+	system("cls");
+	//create top walls
+	for(int i=0; i<width+2; i++)
+		cout<<"-";
+		cout<<endl;
 
-class Snake{
-	private:
-		Point * cell[MAXSNAKESIZE];
-		int size;
-		char dir;
-		Point fruit;
-		public:
-		Snake(){
-			size = 1;
-			cell[0]=new Point(20,20);
-			for(int i=1; i<MAXSNAKESIZE; i++){
-				cell[i]=NULL;
+		for(int i=0; i<height; i++){
+			for(int j=0; j<=width; j++){
+				if(j==0 || j==width)
+					cout<<"|";
+				if(i==y && j==x)
+					cout<<"O";
+				else if(i==fruitY && j==fruitX)
+					cout<<"#";
+				else{
+					bool prTail = false;
+					for(int k=0; k<snakeTailLength; k++){
+						if(snakeTailX[k] == j && snakeTailY[k]==i){
+							cout<<"o";
+							prTail = true;
+						}
+					}
+					if(!prTail)
+						cout<<" ";
+				}
 			}
-			fruit.SetPoint(rand()%50, rand()%25);
+			cout<<endl;
 		}
-		void AddCell(int x, int y){
-			cell[size++]= new Point(x,y);
-		}	
-		void TurnUp(){
-			dir = 'w';
+		//create bottom walls
+		for(int i=0; i<width+2; i++)
+			cout<<"-";
+			cout<<endl;
+		//display score
+		cout<<playerName<<" Score : "<<playerScore<<endl;
+}
+	
+//update game state
+void UpdateGame(){
+	int prevX = snakeTailX[0];
+	int prevY = snakeTailY[0];
+	int prev2X,prev2Y;
+	snakeTailX[0] = x;
+	snakeTailY[0]= y;
+	
+	for(int i=1; i<snakeTailLength; i++){
+		prev2X = snakeTailX[i];
+		prev2Y = snakeTailY[i];
+		snakeTailX[i] = prevX;
+		snakeTailY[i] = prevY;
+		prevX=prev2X;
+		prevY=prev2Y;
+	}
+	switch(sDir){
+		case LEFT:
+			x--;
+			break;
+		case RIGHT:
+			x++;
+			break;
+		case UP:
+			y--;
+			break;
+		case DOWN:
+			y++;
+			break;
+	}
+	if(x>=width || x<0 || y>=height || y<0)
+		isGameOver = true;
+		
+	for(int i=0; i<snakeTailLength; i++){
+		if(snakeTailX[i] == x && snakeTailY[i] == y)
+			isGameOver = true;
+	}
+	if(x == fruitX && y ==fruitY){
+		playerScore += 10;
+		fruitX = rand()%width;
+		fruitY = rand()%height;
+		snakeTailLength++;
+	}
+}
+
+//select difficulty
+int SetDifficulty(){
+	int dif, choice;
+	cout<<"\nSET DIFFICULTY\n1: Easy\n2: Medium\n3: Hard "
+		  "\nNOTE:if not choosen or pressed any other "
+		  "key, the difficulty will be automatically set "
+		  "to medium\nChoose difficulty level: ";
+	cin>>choice;
+	switch(choice){
+		case '1':
+			dif=50;
+			break;
+		case '2':
+			dif=100;
+			break;
+		case '3':
+			dif=150;
+			break;
+		default:
+			dif=100;
+	}
+	return dif;
+}
+
+//handle user inputs
+void UserInput(){
+	if(_kbhit()){
+		switch(_getch()){
+			case 'a':
+				sDir = LEFT;
+				break;
+			case 'd':
+				sDir = RIGHT;
+				break;
+			case 'w':
+				sDir = UP;
+				break;
+			case 's':
+				sDir = DOWN;
+				break;	
+			case 'x':
+				isGameOver = true;
+				break;
 		}
-		void TurnDown(){
-			dir = 's';
-		}
-		void TurnLeft(){
-			dir = 'a';
-		}
-		void TurnRight(){
-			dir = 'd';
-		}
-		void Move(){
-			system("cls");
-			
-			for(int i=size-1; i>0; i--){
-				cell[i-1] -> CopyPos(cell[i]);
-			}
-			
-			switch(dir){
-				case 'w':
-					cell[0] -> MoveUp();
-					break;
-				case 's':
-					cell[0] -> MoveDown();
-					break;
-				case 'a':
-					cell[0] -> MoveLeft();
-					break;
-				case 'd':
-					cell[0] -> MoveRight();
-					break;
-			}
-			if(fruit.GetX() == cell[0]->GetX() && fruit.GetY() ==cell[0]->GetY()){
-				AddCell(0,0);
-				fruit.SetPoint(rand()%50, rand()%25);
-			}
-			for(int i=0; i<size; i++)
-				cell[i] -> Draw();
-			fruit.Draw();
-			Sleep(100);
-		}
-		void Debug(){
-			for(int i=0; i<size; i++){
-				cell[i]->Debug();
-			}
-		}
-};
+	}
+}
 
 int main(){
-	srand((unsigned)time(NULL));
+	string playerName;
+	cout<<"...................................................................\n";
+    cout<<"...................................................................\n";
+	cout<<"..                                                __      __     .. \n";
+	cout<<"..    * * *  **    *      *      *     *  * * * * \\ \\    / /     ..\n";
+	cout<<"..   *       * *   *     * *     *   *    *        \\ \\  / /      .. \n";
+	cout<<"..    * *    *  *  *    * * *    * *      * * *     \\ \\/ /       ..\n";
+	cout<<"..        *  *   * *   *     *   *   *    *         / /\\ \\       ..\n";
+	cout<<"..   * * *   *    **  *       *  *     *  * * * *  / /  \\ \\      ..\n";
+	cout<<"..                                                /_/    \\_\\     ..\n";
+	cout<<"...................................................................\n";
+	cout<<"...................................................................\n";
+	cout<<endl;
+	cout<<endl;
+	cout<<"Enter your name : ";
+	cin>>playerName;
+	int dif = SetDifficulty();
 	
-	Snake snake;
-	char op = 'l';
-	do{
-		if(kbhit()){
-			op = getch();
-		}
-		switch(op){
-			case 'w':
-			case 'W':
-				snake.TurnUp();
-				break;
-				
-			case 's':
-			case 'S':
-				snake.TurnDown();
-				break;
-				
-			case 'a':
-			case 'A':
-				snake.TurnLeft();
-				break;
-				
-			case 'd':
-			case 'D':
-				snake.TurnRight();
-				break;
-		}
-		snake.Move();
-	}while(op != 'e');
-	
+	Game();
+	while(!isGameOver){
+		GameRender(playerName);
+		UserInput();
+		UpdateGame();
+		Sleep(dif);
+	}
 	return 0;
 }
+
